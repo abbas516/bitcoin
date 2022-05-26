e0363f0569f8086815530190a019885f17d29bde
abbas516 <97820922+abbas516@users.noreply.github.com>
2022-05-27 01:15:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def predict():
     output = round(prediction [0],1)
     
 
-    return render_template('login.html', prediction_text='{}'.format(output))
+    return render_template('index.html', prediction_text='{}'.format(output))
 
 if __name__ == "__main__":
     app.run(debug=True)
